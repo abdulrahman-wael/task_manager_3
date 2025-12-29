635f7e4e5cb72df7556e8a21430d26955a68364d
abdulrahman_wael <abdelrahman.wael111@gmail.com>
2025-12-29 11:33:54 +0200

    back and front are working

---
+++ api.py
@@ -0,0 +1,81 @@
+from os import getenv
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+from typing import Annotated
+from fastapi import FastAPI, Depends, Query, HTTPException
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+
+load_dotenv()
+
+if getenv("DATABASE_URL") is None:
+    raise ValueError("DATABASE_URL env var cannot be None")
+
+class BaseTask(BaseModel):
+    title: str
+    description: str | None = None
+    completed: bool = False
+class Task(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field()
+    description: str | None = Field(default=None)
+    completed: bool = Field(default=False)
+class CreateTask(BaseTask):
+    pass
+class UpdateTask(BaseTask):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+
+engine = create_engine(getenv("DATABASE_URL"))
+def get_one_session():
+    with Session(engine) as session:
+        yield session
+sessionDep = Annotated[Session, Depends(get_one_session)]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+app = FastAPI(debug=True, lifespan=lifespan)
+
+@app.post('/tasks/')
+async def create_one_task(session:sessionDep, input_task: CreateTask)->Task:
+    task = Task.model_validate(input_task)
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+@app.get('/tasks/')
+async def read_all_tasks(session:sessionDep, limit=Query(default=100, le=100), offset:int=0)->list[Task]:
+    tasks = session.exec(select(Task).limit(limit).offset(offset))
+    return tasks
+
+@app.get('/tasks/{id}')
+async def read_one_task(session:sessionDep, id:int)->Task:
+    task = session.get(Task, id)
+    if not task:
+        raise HTTPException(status_code=404 , detail=f"task id {id} is not available")
+    return task
+
+@app.patch('/tasks/{id}')
+async def update_one_task(session:sessionDep, id:int, changed_fields:UpdateTask) ->Task:
+    task = session.get(Task, id)
+    if not task:
+        raise HTTPException(status_code=404 , detail=f"task id {id} is not available")
+    dump = changed_fields.model_dump(exclude_unset=True) # didn't add "exclude_unset" from the beginning
+    task.sqlmodel_update(dump)
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+@app.delete('/tasks/{id}')
+async def delete_one_task(session:sessionDep, id:int)->dict:
+    task = session.get(Task, id)
+    if not task:
+        raise HTTPException(status_code=404 , detail=f"task id {id} is not available")
+    session.delete(task)
+    session.commit()
+    return {'status':'ok'}
---
+++ app.py
@@ -0,0 +1,49 @@
+from os import getenv
+from dotenv import load_dotenv
+import streamlit as st
+import requests as rq
+
+load_dotenv()
+
+st.set_page_config(page_icon='👌', page_title='theFROGtask')
+
+with st.sidebar:
+    title = st.text_input(label='Title', max_chars=200, placeholder='what do you want?')
+    description = st.text_area(label='Description', placeholder='write any details ...')
+    submit = st.button(type="primary", label="create")
+    if submit:
+        if not title.strip():
+            st.error("Title cannot be empty")
+        request = rq.post(getenv("API_URL"), json={"title":title, "description":description})
+        if request.status_code == 200:
+            st.success(f"Task with ID {request.json()["id"]} is successfully created")
+
+st.title("FROGGY MORNING!")
+col1, col2 = st.columns([2,1])
+with col1: 
+    limit = st.slider("displyed tasks limit", min_value=5, max_value=100, value=20)
+with col2:
+    show_completed = st.checkbox("show completed tasks", value=False)
+
+all_tasks = rq.get(getenv('API_URL')).json()
+for task in all_tasks:
+    if task['completed'] and not show_completed:
+        continue
+    with st.expander(f"[ {task["id"]} ] {task['title']}", icon=f'{'👌' if task['completed'] else '❄️'}'):
+        col1, col2, col3 = st.columns([3,1,1])
+        with col1: 
+            description = st.write(f"{task['description'] if task['description'] else "nothing special."}")
+        with col2:
+            task_checkbox = st.button(f"{"Done!" if not task["completed"] else "Not done yet!"}", key=f'toggle_task_{task['id']}')
+            if task_checkbox:
+                update_request = rq.patch(url=f"{getenv('API_URL')}{task['id']}",json={"completed": not task['completed']})
+                print(update_request.json()['id'])
+                if update_request.status_code == 200:
+                    st.rerun()
+        with col3:
+            delete_button = st.button("delete", key=f"delete_task_{task['id']}")
+            if delete_button:
+                delete_request = rq.delete(f"{getenv('API_URL')}{task['id']}")
+                if delete_request.status_code == 200:
+                    st.rerun()
+
